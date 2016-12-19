09a8a683132eb640798f3fcd09dd9bb3b6e3405b
Paul Carrick <paulc@orcasnet.com>
2016-12-19 12:39:11 -0800

    Fix paste error in production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
